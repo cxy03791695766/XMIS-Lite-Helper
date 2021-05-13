fecd24f83e6ecd657ee80a06e9d3e38580386767
David Chen <david.chen@xtx.com>
2021-05-13 14:43:21 +0800

    conf

--- source/conf.py
+++ source/conf.py
@@ -38,7 +38,6 @@ release = '1.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx_markdown_tables',
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
